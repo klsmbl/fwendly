459757570998b78ec2977328444d7545e4dfadfd
klsmbl <kcsimbol@student.hau.edu.ph>
2025-10-06 21:39:57 +0800

    finallssss

--- fwendly/fwendly_conwection/urls.py
+++ fwendly/fwendly_conwection/urls.py
@@ -18,4 +18,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    # add static and media files
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
