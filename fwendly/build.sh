44da2cedac5290402d5b88c1432c809763705afd
klsmbl <kcsimbol@student.hau.edu.ph>
2025-10-06 21:46:52 +0800

    finalllssss

---
+++ fwendly/build.sh
@@ -0,0 +1,8 @@
+
+#! /bin/bash
+
+echo "Installing dependencies..."
+pip install -r requirements.txt 
+
+echo "Applying migrations and collecting static files..."
+python manage.py collectstatic --noinput && python manage.py migrate
