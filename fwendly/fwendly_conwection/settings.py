0db194cb387000e84654c6db28f9d42b970ff86c
klsmbl <kcsimbol@student.hau.edu.ph>
2025-10-16 20:42:41 +0800

    added admin acc

--- fwendly/fwendly_conwection/settings.py
+++ fwendly/fwendly_conwection/settings.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dev-secret-key-change-this')
 DEBUG = True
-ALLOWED_HOSTS = ['fwendly-3.onrender.com']
+ALLOWED_HOSTS = ['fwendly-3.onrender.com', '127.0.0.1', 'localhost']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
